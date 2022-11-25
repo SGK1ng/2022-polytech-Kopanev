4a08254db034450105d3ffa07dca7aec23b30504
SGK1ng <ser2065@bk.ru>
2022-11-25 14:20:10 +0300

    fix: linear.cpp

---
+++ 04_search_sort/linear/linear.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int search(int arr[], int n, int x, int result[]){
+    int a = 0;
+    for (int i = 0; i <= n; i++){
+        if (arr[i] == x){
+            result[a] = i;
+            a++;
+        }
+    }
+    return a;
+}
+
+int main() {
+    int m[11] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19}; 
+    const int arrsize = sizeof(m) / sizeof(m[0]);
+    int result[arrsize];
+    for (int i = 0; i < arrsize; ++i){
+        result[i] = 0;
+    }
+    cout << "Count: " << search(m, arrsize, 19, result) << endl << "id: " << endl; // выводим количество найденных элементов и их номера
+    for (int i = 0; i < arrsize; ++i){
+        if (result[i] != 0) {
+            cout << result[i] << endl;
+        }
+    }
+}
